c2cd8861344bea88e4761a7df5ee407f3e2a43a4
Yan Lytovka <yanl@jfrog.com>
2025-05-19 14:22:52 +0200

    [install.sh] add Istio version checks and CRD installation prompts
    - Introduce minimum and maximum required Istio versions.
    - Implement version comparison function to validate Istio version.
    - Add checks for Istio CRDs and prompt for installation if missing.
    - Ensure required tools (kubectl, helm, yq) are checked before proceeding.
    - Create a namespace if it does not already exist.

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
+MIN_REQUIRED_ISTIO_VERSION="1.18"
+MAX_REQUIRED_ISTIO_VERSION="1.24.3"
+
 # ASCII Art for welcome sign
 ascii_art='
 
@@ -39,11 +42,38 @@ CRD_URLS=(
   "https://raw.githubusercontent.com/qwak-ai/jfrogml-helm-chart-crds/main/crds/crd-podmonitors.yaml"
   "https://raw.githubusercontent.com/qwak-ai/jfrogml-helm-chart-crds/main/crds/crd-servicemonitors.yaml"
   "https://raw.githubusercontent.com/qwak-ai/jfrogml-helm-chart-crds/main/crds/crd-prometheusrules.yaml"
-  "https://raw.githubusercontent.com/qwak-ai/jfrogml-helm-chart-crds/main/crds/istio.yaml"
   "https://raw.githubusercontent.com/qwak-ai/jfrogml-helm-chart-crds/main/crds/kafka.yaml"
   "https://raw.githubusercontent.com/qwak-ai/jfrogml-helm-chart-crds/main/crds/victoria-metrics.yaml"
   "https://raw.githubusercontent.com/qwak-ai/jfrogml-helm-chart-crds/main/crds/elasticsearch.yaml"
+  #"https://raw.githubusercontent.com/qwak-ai/jfrogml-helm-chart-crds/main/crds/istio.yaml"
 )
+
+compare_versions() {
+    if [[ "$1" == "$2" ]]; then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++)); do
+        ver1[i]=0
+    done
+    
+    for ((i=${#ver2[@]}; i<${#ver1[@]}; i++)); do
+        ver2[i]=0
+    done
+    
+    for ((i=0; i<${#ver1[@]}; i++)); do
+        if ((10#${ver1[i]} > 10#${ver2[i]})); then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]})); then
+            return 2
+        fi
+    done
+    
+    return 0
+}
 
 kubectl_check() {
   if ! command -v kubectl &> /dev/null; then
@@ -53,26 +83,85 @@ kubectl_check() {
   fi
 }
 
-#!/bin/bash
+helm_check() {
+  if ! command -v helm &> /dev/null; then
+    echo -e "${RED}ERROR: helm is not installed or not in PATH.${NC}"
+    echo "Please install helm by following the instructions here: https://helm.sh/docs/intro/install/"
+    exit 1
+  fi
+}
+
+yq_check() {
+  if ! command -v yq &> /dev/null; then
+    echo -e "${RED}ERROR: yq is not installed or not in PATH.${NC}"
+    echo "Please install yq by following the instructions here: https://github.com/mikefarah/yq#install"
+    exit 1
+  fi
+}
+
+check_required_tools (){
+  kubectl_check
+  helm_check
+  yq_check
+}
+
+check_istio_version() {
+  # Retrieving all images from all namespaces
+  images=$(kubectl get pods --all-namespaces -o jsonpath="{..image}" |\
+           tr -s '[[:space:]]' '\n' |\
+           sort | uniq)
+  
+  # Filtering Istio images
+  istio_images=$(echo "${images}" | grep -i istio)
+  if [ -z "$istio_images" ]; then
+    echo "No Istio images found in the cluster."
+    return
+  fi
+  # DEBUG
+  #echo "Istio images with versions found in the cluster:"
+  #echo "==============================================="
+  #echo "${istio_images}"
+  #echo
+  
+  # Extract image tags
+  tags=$(echo "$istio_images" | awk -F: '{print $NF}' | sort | uniq)
+
+  if [ "$(echo "$tags" | wc -l)" -eq 1 ]; then
+    version=$(echo "$tags" | head -n 1)
+    echo -e "Probably you are running Istio version: ${GREEN}${version}${NC}"
+    compare_versions $version $MIN_REQUIRED_ISTIO_VERSION
+    is_min_version_ok=$?
+    
+    compare_versions $version $MAX_REQUIRED_ISTIO_VERSION
+    is_max_version_ok=$?
+    
+    if [[ $is_min_version_ok -ne 2 && $is_max_version_ok -ne 1 ]]; then
+        echo "Your version $version is within the required range."
+    else
+        echo "Your version $version is not within the required range."
+    fi
+      else
+        echo "Multiple Istio versions detected:"
+        echo -e "Try to reinstall istio and use 1 versoin from : $MIN_REQUIRED_ISTIO_VERSION to $MAX_REQUIRED_ISTIO_VERSION"
+        exit 1
+        echo "${tags}"
+      fi
+}
 
 check_k8s_context() {
     # Get the current Kubernetes context
     current_context=$(kubectl config current-context)
-    
     if [ -z "$current_context" ]; then
         echo "No current Kubernetes context found."
         return 1
     fi
-    
     # Display the current context
-    echo -e "Current Kubernetes context: '${GREEN}$current_context${NC}'"
-    
+    echo -e "Current Kubernetes context: '${YELLOW}$current_context${NC}'"
     # Prompt the user to confirm if it is the correct context
     read -p "Is this the correct context? (y/n): " user_input
-    
     case $user_input in
         [Yy]*)
-            echo -e "${GREEN}Context confirmed.${NC}"
+            echo -e "Context: ${GREEN}$current_context${NC} confirmed"
             ;;
         [Nn]*)
             echo -e "Context not confirmed. Please update the context using '${YELLOW}kubectl config use-context <context-name>'${NC}."
@@ -80,62 +169,43 @@ check_k8s_context() {
             ;;
         *)
             echo -e "Invalid input. Please enter ${GREEN}'y'${NC} for yes or ${RED}'n'${NC} for no."
+            exit 1
             ;;
     esac
 }
-
-create_namespace() {
-  local namespace=$1
-  if kubectl get namespace "$namespace" &> /dev/null; then
-    echo -e "${YELLOW}Warning: Namespace '$namespace' already exists.${NC}"
-  else
-    echo "Creating namespace '$namespace'"
-    kubectl create namespace "$namespace"
-    echo -e "${GREEN}Namespace '$namespace' created successfully.${NC}"
-  fi
-}
-
 
 # Function to extract a resource from the CRD file by name using yq
 extract_resource_by_name() {
   local crd_file="$1"
   local resource_name="$2"
-
   if [ -z "$crd_file" ] || [ -z "$resource_name" ]; then
     echo "Usage: extract_resource_by_name <crd_file> <resource_name>"
     return 1
   fi
-
   # Check if the CRD file exists
   if [ ! -f "$crd_file" ]; then
     echo "CRD file not found: $crd_file"
     return 1
   fi
-
   # Use yq to filter the resource by name and print the resource
   yq eval-all "
     select(has(\"metadata\") and .metadata.name == \"$resource_name\")
   " "$crd_file"
 }
-
 # Function to install CRD
 install_crd() {
   local crd_url="$1"
   local resource_name="$2"
-
   #echo -e "Applying CRD from: ${GREEN}$crd_url${NC}"
-  
   # Download the CRD file
   local crd_file=$(mktemp)
   curl -s -o "$crd_file" "$crd_url"
-
   if [ -z "$resource_name" ]; then
     # If no resource name is provided, apply the entire CRD manifest
     kubectl apply -f "$crd_file"
   else
     # Extract and apply the specific resource from the CRD manifest
     local extracted_resource=$(extract_resource_by_name "$crd_file" "$resource_name")
-
     if [ -n "$extracted_resource" ]; then
       echo "$extracted_resource" | kubectl apply -f -
     else
@@ -143,7 +213,6 @@ install_crd() {
       return 1
     fi
   fi
-
   # Remove the temporary file
   rm -f "$crd_file"
 }
@@ -163,36 +232,103 @@ fetch_and_parse_crd() {
     if kubectl get crd "$resource" > /dev/null 2>&1; then
       echo -e "CRD '${GREEN}$resource${NC}' exists in the cluster."
     else
-      echo -e "CRD '${RED}$resource${NC}' is missing in the cluster "
-      # Prompt the user to confirm if it is the correct context
-      read -p "Do you want to install $resource? (y/n): " user_input
-    
-    case $user_input in
-        [Yy]*)
-            install_crd "$url" "$resource" && echo -e "CRD '${GREEN}$resource${NC}' installed successfully"
-            ;;
-        [Nn]*)
-            echo -e "${RED}Installation is canceled${NC}."
-            exit 1
-            ;;
-        *)
-            echo -e "Invalid input. Please enter ${GREEN}'y'${NC} for yes or ${RED}'n'${NC} for no."
-            ;;
-    esac
-
+      echo -e "CRD '${RED}$resource${NC}' is missing in the cluster."
+      MISSING_CRDS+=("$resource#$url")
     fi
   done
 }
 
-main() {
-  kubectl_check
-  check_k8s_context
+check_istio_crds() {
+  check_istio_version
+
+  echo "Checking Istio CRDs..."
+  ISTIO_URL="https://raw.githubusercontent.com/qwak-ai/jfrogml-helm-chart-crds/main/crds/istio.yaml"
+  local istio_missing_crds=()
+  # Fetch and parse Istio CRDs
+  fetch_and_parse_crd "$ISTIO_URL"
+  for crd in "${resources[@]}"; do
+    if ! kubectl get crd "$crd" > /dev/null 2>&1; then
+      echo -e "Istio CRD '${RED}$crd${NC}' is missing."
+      istio_missing_crds+=("$crd")
+    fi
+  done
+
+  if [ ${#istio_missing_crds[@]} -gt 0 ]; then
+    read -p "Do you want to install Istio CRDs? (y/n): " user_input
+    case $user_input in
+      [Yy]*)
+        echo "Installing Istio CRDs..."
+        for crd in "${istio_missing_crds[@]}"; do
+          install_crd "$ISTIO_URL" "$crd" && echo -e "Istio CRD '${GREEN}$crd${NC}' installed successfully"
+        done
+        ;;
+      [Nn]*)
+        echo -e "${RED}Installation canceled. Please install Istio CRDs for the required version or run the installation script again and accept the installation.${NC}"
+        exit 1
+        ;;
+      *)
+        echo -e "Invalid input. Please enter ${GREEN}'y'${NC} for yes or ${RED}'n'${NC} for no."
+        exit 1
+        ;;
+    esac
+  else
+    echo -e "${GREEN}All required Istio CRDs are present.${NC}"
+  fi
+}
+
+check_others_crds() {
   echo "Checking installed CRDs..."
+  declare -a MISSING_CRDS
   for url in "${CRD_URLS[@]}"; do
     fetch_and_parse_crd "$url"
   done
-  create_namespace "jfrogml"
+  if [ ${#MISSING_CRDS[@]} -gt 0 ]; then
+    echo -e "\nThe following CRDs are missing and need to be installed:"
+    for item in "${MISSING_CRDS[@]}"; do
+      IFS="#" read -r resource _ <<< "$item"
+      echo -e "- ${resource}"
+    done
+    read -p "Do you want to install these missing CRDs? (y/n): " user_input
+    case $user_input in
+        [Yy]*)
+            for item in "${MISSING_CRDS[@]}"; do
+              IFS="#" read -r resource url <<< "$item"
+              install_crd "$url" "$resource" && echo -e "CRD '${GREEN}$resource${NC}' installed successfully"
+            done
+            ;;
+        [Nn]*)
+            echo -e "${RED}Installation is canceled.${NC}"
+            exit 1
+            ;;
+        *)
+            echo -e "Invalid input. Please enter ${GREEN}'y'${NC} for yes or ${RED}'n'${NC} for no."
+            exit 1
+            ;;
+    esac
+  else
+    echo -e "${GREEN}No missing CRDs detected.${NC}"
+  fi
+}
+
+check_jfrogml_namespace() {
+  local namespace="jfrogml"
+  if kubectl get namespace "$namespace" &> /dev/null; then
+    echo -e "${YELLOW}Note: Namespace '$namespace' already exists.${NC}"
+  else
+    echo "Creating namespace '$namespace'"
+    kubectl create namespace "$namespace"
+    echo -e "${GREEN}Namespace '$namespace' created successfully.${NC}"
+  fi
   echo -e "${GREEN}Installation script finished successfully.${NC}"
 }
 
+main() {
+  check_required_tools
+  echo -e "\n"
+  check_k8s_context
+  echo -e "\n"
+  check_istio_crds
+  echo -e "\n"
+  check_jfrogml_namespace
+}
 main
