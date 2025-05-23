e3696988a05c68c26781f0f85b6353c42283bf5d
Yan Lytovka <yanl@jfrog.com>
2025-05-23 18:10:15 +0200

    [install.sh] enable AWS LB controller check
    Remove the comment from the AWS Load Balancer controller installation
    check to ensure it is executed during the installation process.
    
    - This change allows for proper validation of the AWS LB controller
      installation.
    - Ensures that any issues with the controller can be detected early
      in the installation flow.

--- install.sh
+++ install.sh
@@ -452,7 +452,7 @@ main() {
   echo -e "\n"
   check_k8s_context
   echo -e "\n"
-  #is_aws_lb_controller_installed
+  is_aws_lb_controller_installed
   echo -e "\n"
   check_others_crds
   echo -e "\n"
